988a174407f607c9ae7f4c82ac34b932e3858412
Derville Julien <ju.derville@gmail.com>
2015-07-13 15:40:19 +0200

    Assignment result
    Result of the assignment for coursera

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse
+             )
 }
 
 
@@ -12,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        
+        if(!is.null(m)){
+        return(im)
+        }
+        
+        new <- x$get()
+        m <- solve(new, ...)
+        x$setinverse(m)
 }
+
+
